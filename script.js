181a2794fd457b9d6d01e9526f633cde8792d1fd
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-09-17 14:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+}
+
+.game-container {
+    display: none;
+}
+
+#game-result,
+#round-result {
+    display: none;
+}
